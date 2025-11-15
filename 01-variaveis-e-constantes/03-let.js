db6ded6236b34d93a400bb4db934110e6237c895
Igor Santos <igorlimasantos667@gmail.com>
2025-11-15 13:49:25 -0300

    let

---
+++ 01-variaveis-e-constantes/03-let.js
@@ -0,0 +1,14 @@
+//Declara uma váriavel sem valor
+
+let user ;  // Cria uma variavel user sem valor 
+console.log("Igor Lima"); // vai mostrar no terminal o valor igor Lima , esse console.log não esta ligado ao user
+
+//var user = "Igor Lima"; // analisar isso , sobre consistencia de var e let quando sobreescreve
+
+
+let email = "igorlima@gmail.com"/// cria uma variavel email com o valor igorlima@gmail.com
+console.log(email); //mostra no terminal o valor da variavel email
+
+email = "igorlimasantos@gmail.com"; // fez uma nova atribuição a variavel email com um novo valor
+console.log(email);// mostra o novo valor da variavel email no Terminal 
+
