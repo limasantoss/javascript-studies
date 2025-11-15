7198bd773519326fa906b15ecfe6ff2d7629ad27
Igor Santos <igorlimasantos667@gmail.com>
2025-11-15 09:15:13 -0300

    var

---
+++ 01-variaveis-e-constantes/01-var.js
@@ -0,0 +1,17 @@
+
+//Declara uma variável sem valor
+var user // Uma variavel sem valor . Declara uma variavel chamada user e sem valor
+
+console.log(user); // Mostra no console o valor da variavel user que vai dar undefined , pois não teve o valor declarado 
+
+
+//Declara  uma variável com valor´
+var email = "igorlima@gmail.com" // Declara uma variavel var chamada email . não vai dar undefined , pois agora tem um valor Definido 
+console.log(email); // Mostra no console o valor da variavel email  , que é o valor Declarado 
+
+
+//Substitui o valor da variável
+email = "igorlimasantos@gmail.com"; //agora ele altera(atualiza) o valor da variavel email 
+console.log(email); //Mostra no terminal o valor da variavel email ja atualizada
+
+
