942ef111a51a2cdebc8435feb1baa6409335916c
Igor Santos <igorlimasantos667@gmail.com>
2025-11-15 17:33:32 -0300

    Const

---
+++ 01-variaveis-e-constantes/04-const.js
@@ -0,0 +1,9 @@
+//Uma constante é uma variável cujo valor não pode ser alterado após sua atribuição inicial.    
+const number = 42;//
+console.log(number);
+
+
+// Isso causará um erro, pois não se pode reatribuir uma constante.
+number = 55     
+console.log(number); 
+
