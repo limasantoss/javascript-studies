fbe6b5a6cee18cd3aceb4afb08a34f7194232c41
Igor Santos <igorlimasantos667@gmail.com>
2025-11-15 11:17:20 -0300

    Case Sensitive

---
+++ 01-variaveis-e-constantes/02-caseSensitive.js
@@ -0,0 +1,18 @@
+/*
+Quando uma linguagem de programação é case sensitive,
+ isso significa que ela diferencia letras maiúsculas de minúsculas.
+
+ Por exemplo, em JavaScript, as variáveis "User",
+  "user" e "USER"
+*/
+
+
+var product = "Teclado Mecânico"; //Criou uma variavel product onde esta sendo o valor atribuado como teclado mecanico 
+product = "Monitor 4K";// reatribuiu o valor da variavel product para um novo valor como Monitor 4k
+var Product = "Mouse Gamer"; // Criou uma nova variavel chamada Product , tem o mesmo nome porem essa ja começa com o P maiusculo 
+// e esse tema que é abordado na Aula a diferença entre letras Maiusculas e letras Minusculas , quando se atribui uma Variavel 
+// e para essa variavel esta recebendo o valor Mouse Gamer 
+console.log(product);
+//Agora no terminal vai mostrar o valor da variavel product que é monitor 4k
+console.log(Product);
+// Agora no terminal vai mostrar o valor da variavel Product que é Mouse Gamer
