b84304bd99ad41427b39ca2a9d480ee1f88c7e2f
Igor Santos <igorlimasantos667@gmail.com>
2025-11-15 21:47:03 -0300

    String

---
+++ 01-variaveis-e-constantes/09-string.js
@@ -0,0 +1,18 @@
+//String
+let username = "Igor lima ";
+console.log(username);
+console.log(typeof username);
+
+console.log("Uma string com Aspas");
+console.log("Uma string com aspas Simples");
+
+//Quando utilizar uma na outra ?
+console.log('Uma string com "Aspas Duplas" dentro de Simples');
+
+console.log('Uma string com "Aspas duplas " dentro de aspas simples');
+
+console.log(`
+        Uma string com  acento grave que permite escrever 
+        multiplas linhas (tabulação )
+    
+    `);
