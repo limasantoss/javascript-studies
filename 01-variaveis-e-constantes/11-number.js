e394e6579fe2bc678d4340969b8be7ff660208c5
Igor Santos <igorlimasantos667@gmail.com>
2025-11-17 14:17:41 -0300

    number

---
+++ 01-variaveis-e-constantes/11-number.js
@@ -0,0 +1,14 @@
+//Number 
+console.log( typeof 5);
+
+//inteiro Positivo
+console.log(5);
+
+//Inteiro Negativo
+console.log(-5);
+
+//Numero reais ou float
+console.log(125.70);
+
+//Nan - not a Number.
+console.log(12.5 / "Igor Lima");
