9fd51dbfc8b0ba5e389121a678dc723f54677897
Igor Santos <igorlimasantos667@gmail.com>
2025-11-18 17:32:24 -0300

    sintaxe

---
+++ javascript/03sintaxe/aula.js
@@ -0,0 +1,9 @@
+var a = 1;
+// criei uma variavel  a que esta recebendo o valor de 1
+var b = 2;
+//criei uma variavel  b que esta recebendo o valor de 2
+var resultado = a + b
+// criei uma variavel resultado que esta recebendo o valor de a + b . entao ela só vai receber o resultado desse valor 
+console.log(resultado)
+// aqui estou utilizando o console.e sua propriedade log para poder mostrar o resultado no console ou no terminal 
+
