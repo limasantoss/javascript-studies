4b7be9d5fcef42d490dbcd16db027287b270ee16
Igor Santos <igorlimasantos667@gmail.com>
2025-11-18 17:27:27 -0300

    declarações

--- javascript/02declaracoes/aula.js
+++ javascript/02declaracoes/aula.js
@@ -1,2 +1,6 @@
 document.getElementById("script").innerHTML= "Aula de Declarações"
+// Aqui estou utilizando o dom e utilizando a propriedade para capturar o Id DA TAG HTML 
+//CAPTUREI O ID AGORA VOU UTILIZAR A PROPRIEDADE INNERHTML PARA PODER INCLUIR ESSE TEXTO NA TAG ONDE POSSUI O ID QUE CAPTURAMOS 
 document.getElementById("paragrafo").innerHTML = "Nessa aula foi ensinado como declara um script! "
+//AQUI ESTOU UTILIZANDO O DOM E SUA PROPRIEDADE PARA CAPTURAR O ID PARAGRAFO 
+// CAPTUREI O ID AGORA VOU UTILIZAR A PROPRIEDADE PARA PODER INCLUIR TEXTO NESSA TAG 
